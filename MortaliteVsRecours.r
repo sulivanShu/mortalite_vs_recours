40eba4d3aea592472c4f1a05321cd8ae859ea570
Sylvain Rousselot <regivanx@gmail.com>
2023-07-02 18:17:10 +0200

    Add many comments in uk/chunks.r. Format and clarify MortaliteVsRecours.r. subset() instead `[`()

--- MortaliteVsRecours.r
+++ MortaliteVsRecours.r
@@ -6,34 +6,64 @@ library(weights)
 
 # Évolution du taux de mortalité standardisé de 2020 par rapport à la moyenne des trois années précédantes.
 # Usage de la bibliothèque insee. Sa documentation se trouve ici: https://cran.r-project.org/web/packages/insee/insee.pdf
-EvoMortStd = get_idbank_list("DECES-MORTALITE") |> 
-	subset(FREQ == "A" & INDICATEUR == "TAUX_MORTALITE_STANDARDISE" & grepl("^D", REF_AREA) & AGE == "65-") |> 
-	# data.frame des idbank
-	`[`(x = _, "idbank") |> 
+EvoMortStd = 
+	get_idbank_list("DECES-MORTALITE") |> 
+	subset(
+	       subset =
+		       FREQ == "A" & 
+		       INDICATEUR == "TAUX_MORTALITE_STANDARDISE" & 
+		       grepl("^D", REF_AREA) & 
+		       AGE == "65-", 
+	       select = 
+		       "idbank"
+       ) |> 
 	# convertir le data.frame en character
 	unlist() |> 
 	# sélectionner la période pertinente des tableaux
-	get_insee_idbank(startPeriod = 2017, endPeriod = 2020) |> 
+	get_insee_idbank(
+			 startPeriod = 2017,
+			 endPeriod = 2020
+			 ) |> 
 	# sélectionner les valeurs pertinentes
-	`[`(x = _, "OBS_VALUE") |> 
+	subset(select = "OBS_VALUE") |> 
 	# convertir le data.frame en numeric
 	unlist() |> 
 	unname() |>
 	# fonction pour calculer le taux de croissance de la dernière année par rapport à la moyenne des trois années précédentes
-	(function(data = _) { sapply(seq(1, length(data), by = 4), function(start) { ( data[start] - mean(data[(start+1):(start+3)]) ) / mean(data[(start+1):(start+3)]) }) })() |>
+	(function(data = _) {
+		 sapply(
+			seq( 1, length(data), by = 4),
+			function(start) {
+				( data[start] - mean(data[(start+1):(start+3)]) ) / mean(data[(start+1):(start+3)]) 
+			 }
+			) 
+			 })() |>
 	print()
 
 # Nombre de personnes de plus de 60 ans par département fois 100
-PondPop60 = get_idbank_list("TCRED-ESTIMATIONS-POPULATION") |> 
-	subset(grepl("^D", REF_AREA) & SEXE == "0" & grepl("00-$|60-$", AGE) ) |> 
-	`[`(x = _, "idbank") |> 
+PondPop60 = get_idbank_list("TCRED-ESTIMATIONS-POPULATION") |>
+	subset(
+	       subset = 
+		       grepl("^D", REF_AREA) & 
+		       SEXE == "0" & 
+		       grepl("00-$|60-$", AGE),
+	       select = 
+		       "idbank"
+       ) |> 
 	unlist() |> 
 	get_insee_idbank(startPeriod = 2020, endPeriod = 2020) |> 
 	(\(data) {data[order(data$REF_AREA), ] })() |> 
-	`[`(x = _, "OBS_VALUE") |> 
+	subset(select = "OBS_VALUE") |> 
 	unlist() |> 
 	unname() |>
-	(\(data) { sapply(seq(1, length(data), by = 2), \(start) { (data[start]) * data[start+1] }) })() |>
+	(\(data) { 
+		 sapply(
+			seq(1, length(data), by = 2), 
+			\(start) { 
+				(data[start]) * data[start+1] 
+		       }
+		 ) 
+       })() |>
 	print()
 
 # évolution du taux de patients
@@ -44,16 +74,19 @@ PondPop60 = get_idbank_list("TCRED-ESTIMATIONS-POPULATION") |>
 # usage de la bibliothèque rvest: https://www.rdocumentation.org/packages/rvest/versions/1.0.3
 
 # convertir les caractères de chiffres en nombres
-clean_numeric = \(x) {
-  x = gsub(",", ".", x) |> 
-    gsub(" ", "", x=_) |>
-    as.numeric() |>
-    suppressWarnings()
+clean_numeric = 
+	\(x) {
+	x = gsub(",", ".", x) |> 
+		gsub(" ", "", x=_) |>
+		as.numeric() |>
+		suppressWarnings()
 }
 
-html_page = read_html("https://www.scansante.fr/applications/taux-de-recours-tous-champs/submit?snatnav=&mbout=part1&champ=tous+champs&unite=patients&version=v2021&taux=stand&tgeo=dep")
+html_page =
+	read_html("https://www.scansante.fr/applications/taux-de-recours-tous-champs/submit?snatnav=&mbout=part1&champ=tous+champs&unite=patients&version=v2021&taux=stand&tgeo=dep")
 
-EvoTxPat = html_page |>
+EvoTxPat =
+	html_page |>
 	(\(data) {html_table(data)[[3]]})() |>
 	# nettoyage:
 	(\(df) {
@@ -62,14 +95,14 @@ EvoTxPat = html_page |>
 		 df |> tail(-1) |> head(-3)
 })() |>
 # calcul:
-(\(df) {
-	 sapply(1:nrow(df), \(i) {
-			(df[i, 5] - rowMeans(df[i, 2:4])) / rowMeans(df[i, 2:4])
-} ) |> 
-unlist()
-})() |> 
-unname() |>
-print()
+	(\(df) {
+		 sapply(1:nrow(df), \(i) {
+				(df[i, 5] - rowMeans(df[i, 2:4])) / rowMeans(df[i, 2:4])
+			} ) |> 
+		unlist()
+	})() |> 
+	unname() |>
+	print()
 
 # Corrélation entre l'évolution de la mortalité standardisée et l'évolution du taux de recours aux soins hospitaliers, par département, 2020 par rapport à la moyenne 2017-2019, pondérée la population des plus de 60 ans dans chaque département
 # usage de la bibliothèque weights: https://www.rdocumentation.org/packages/rvest/versions/1.0.3
@@ -79,8 +112,16 @@ wtd.cor(EvoMortStd, EvoTxPat, weight = PondPop60) |>
 # Créer le fichier PNG
 png("EvoMortStdVsEvoTxPat.png")
 # Dessiner le nuage de points
-plot(EvoTxPat, EvoMortStd, cex = PondPop60/10000000, main = "Δ Mortalité standardisée vs Δ recours aux soins, 2020", xlab = "Δ recours aux soins", ylab = "Δ mortalité standardisée")
+plot(
+     x = EvoTxPat,
+     y = EvoMortStd,
+     cex = PondPop60/10000000,
+     main = "Δ Mortalité standardisée vs Δ recours aux soins, 2020",
+     xlab = "Δ recours aux soins",
+     ylab = "Δ mortalité standardisée"
+)
 # Ajuster la régression linéaire et ajouter la ligne de tendance:
-lm(EvoMortStd ~ EvoTxPat, weights = PondPop60) |> abline(col = "red")
+lm(EvoMortStd ~ EvoTxPat, weights = PondPop60) |>
+	abline(col = "red")
 # Fermer le fichier PNG
 dev.off()
--- uk/chunks.r
+++ uk/chunks.r
@@ -1,108 +1,220 @@
+# ---
+# title: "Corrélation entre la baisse du recours aux soins et la hausse de la mortalité en 2020, Royaume-Uni"
+# author: ""
+# date: "2023-07-02"
+# output: html_document
+# ---
+# 
+# # Corrélation entre la baisse du recours aux soins et la hausse de la mortalité en 2020, Royaume-Uni
+# 
+# ## Bibliothèques & API
+# 
+# Il n'existe pas de bibliothèque R similaire à `insee` pour le NHS, l'agence de santé britannique.
+# 
+# Il existe une API, mais son accès est soumise à autorisation.
+# 
+# https://digital.nhs.uk/data-and-information/data-tools-and-services/data-services/hospital-episode-statistics/users-uses-and-access-to-hospital-episode-statistics
+# 
+# ## Sources
+# 
+# ### Recours au soin
+# 
+# https://digital.nhs.uk/data-and-information/publications/statistical/hospital-admitted-patient-care-activity
+# https://digital.nhs.uk/data-and-information/publications/statistical/hospital-admitted-patient-care-activity/2020-21
+# https://digital.nhs.uk/data-and-information/publications/statistical/hospital-admitted-patient-care-activity/2019-20
+# https://digital.nhs.uk/data-and-information/publications/statistical/hospital-admitted-patient-care-activity/2018-19
+# https://digital.nhs.uk/data-and-information/publications/statistical/hospital-admitted-patient-care-activity/2017-18
+# 
+# Section:
+# 
+# Hospital Admitted Patient Care Activity, 2020-21: Provider-level analysis
+# Hospital Admitted Patient Care Activity, 2019-20: Provider-level analysis
+# Hospital Admitted Patient Care Activity, 2018-19: Provider-level analysis
+# Hospital Admitted Patient Care Activity, 2017-18: Provider-level analysis
+# 
+# hosp-epis-stat-admi-hosp-prov-2015-16-tab.xlsx
+# hosp-epis-stat-admi-hosp-prov-2016-17-tab.xlsx
+# hosp-epis-stat-admi-hosp-prov-2020-21-tab.xlsx
+# hosp-epis-stat-admi-hosp-prov-2021-22-tab.xlsx
+# hosp-epis-stat-admi-prov-2017-18-tab.xlsx
+# hosp-epis-stat-admi-prov-2018-19-tab.xlsx
+# hosp-epis-stat-admi-prov-2019-20-tab.xlsx
+# 
+# ces fichiers contiennent le nom de tous les hôpitaux d'Angleterre, leur identifiant, et des indicateurs de recours aux soins. L'indicateur utilisé est "Finished consultant episodes".
+# 
+# ### Code géographique NHS
+# 
+# https://odsdatapoint.digital.nhs.uk/userdefined
+# DataPointSavedGridState.json
+# 
+# ODS_2023-06-20T170741.csv
+# 
+# Ce fichier indique les codes géographiques NHS et postaux des hôpitaux d'Angleterre, à l'exeption de "8A718 London Clinic" et "8HE28 Harley Street On 15" qui doivent être ajoutés manuellement.
+# 
+# selon:
+# 
+# https://odsportal.digital.nhs.uk/Organisation/OrganisationDetails?organisationId=67432&showOpenChildredOnly=True
+# 
+# https://www.streetcheck.co.uk/postcode/w1g6bw
+# 
+# "8A718 London Clinic" a pour autorité administrative: Westminster (ONS: E09000033, NHS: 713)
+# 
+# selon:
+# 
+# https://odsportal.digital.nhs.uk/Organisation/OrganisationDetails?organisationId=75403&showOpenChildredOnly=True
+# 
+# https://www.streetcheck.co.uk/postcode/nw12bu
+# 
+# "8HE28 Harley Street On 15" a pour autorité administrative: Camden (ONS: E09000007, NHS: 702)
+# 
+# # Code géographique ONS
+# 
+# https://www.england.nhs.uk/statistics/statistical-work-areas/delayed-transfers-of-care/delayed-transfers-of-care-data-2019-20/
+# 
+# Total Delayed Days Local Authority 2019-20 February (revised 10.09.2020) (XLS, 138KB)
+# 
+# LA-Type-B-February-2020-4W5PA.xls
+# 
+# Ce fichier comprend la correspondance des codes géographiques NHS avec les codes géographiques ONS, ce qui permet de croiser les données de recours au soins du NHS avec les données de mortalité de l'ONS.
+# 
+# Cette correspondance peut également être effectuée à l'aide des codes postaux des hôpitaux, mis en correspondance avec les codes ONS des autorités administratives, à l'aide d'un autre fichier (non fourni)
+
+
+
 #!/usr/bin/Rscript
 
 library(readxl)
 library(tibble)
 # la librairie tibble n'est pas nécessaire, mais elle est utilisée pour mieux voir les dataframes
 
-# lire le fichier qui contient les indicateurs de recours aux soins hospitaliers
-# l'accès aux bases de données est soumise à autorisation: https://digital.nhs.uk/data-and-information/data-tools-and-services/data-services/hospital-episode-statistics/users-uses-and-access-to-hospital-episode-statistics
-hosp = read_excel("hosp-epis-stat-admi-hosp-prov-2020-21-tab.xlsx")
+# # Correspondances
 
-# éditer une ligne qui servira ensuite de noms pour les colonnes. "Code" est le code NHS de l'établissement.
-hosp[7,c(2,3,49)] = c("Code", "Hospital.provider.name", "Zero.bed.day.cases.Emergency") |>
+# Les objets dont le nom commence par "clean_" sont des objets nettoyés, utilisables dans les calculs
+
+# ## Correspondance `Code` -- `Finished consultant episodes` (Code NHS de l'établissement vs indicateur de recours au soin) 
+# lire le fichier qui contient les indicateurs de recours aux soins hospitaliers
+hosp =
+	read_excel("hosp-epis-stat-admi-hosp-prov-2020-21-tab.xlsx")
+
+# Éditer une ligne qui servira ensuite de noms pour les colonnes. "Code" est le code NHS de l'établissement.
+hosp[7,c(2,3,49)] =
+	c("Code", "Hospital.provider.name", "Zero.bed.day.cases.Emergency") |>
 	as.list() |>
 	print()
 
-# renommer des colonnes
-colnames(hosp) = hosp[7,] |>
+# Renommer des colonnes
+colnames(hosp) =
+	hosp[7,] |>
 	gsub("NA", NA, x=_) |>
 	print()
 
-# nettoyage du fichier. dans le fichier, le caractère * signfie soit un nombre entre 1 et 7, soit "Not available".
-replace_star_integer = \(x) {
-	gsub("\\*", "3", x) |> as.integer() # valeur réelle entre 1 et 7. J'ai choisi 3 pour avoir un entier. Possibilité d'être plus précis.
-}
-replace_star_numeric = \(x) {
-	gsub("\\*", NA, x) |> as.numeric()
-}
-# les codes terminant par "-X" sont des codes de réserve provisoires. Il faut supprimer ce suffixe pour avoir le code normal. reste encore "8A718 London Clinic" et "8HE28 Harley Street On 15" à gérer. Apparemment, ce sont des "Non-Nhs organisations", c'est peut-être pour ça qu'ils n'apparaissent pas dans mon autre fichier. absents de https://odsdatapoint.digital.nhs.uk/userdefined . Présent à https://odsportal.digital.nhs.uk/ . 8HE28 : GREATER LONDON . 8A718 : GREATER LONDON .
-replace_X_code = \(x) {
-	gsub("-X", "", x)
-}
-# lignes 19 à 509, colonnes dont le nom n'est pas NA (colonnes vides)
-clean_hosp = hosp[19:509, hosp |> names() |> (\(x) !is.na(x))()] |>
+# Nettoyage du fichier 
+# Dans le fichier, le caractère * signfie soit un nombre entre 1 et 7, soit "Not available".
+# Fonction pour remplacer le caractère "*" par une valeur numérique. J'ai choisi 3 pour avoir un entier entre 1 et 7. Possibilité d'être plus précis.
+replace_star_integer =
+	\(x) {
+		gsub("\\*", "3", x) |> as.integer() 
+	}
+# Fonction pour remplacer le caratère "*" par la valeur "Not available".
+replace_star_numeric =
+	\(x) {
+		gsub("\\*", NA, x) |> as.numeric()
+	}
+# Les codes se terminant par "-X" sont des codes de réserve provisoires. Il faut supprimer ce suffixe pour avoir le code normal. 
+replace_X_code =
+	\(x) {
+		gsub("-X", "", x)
+	}
+# Sélectionner les cellules utiles du tableau. Lignes 19 à 509, colonnes dont le nom n'est pas NA (colonnes non-vides)
+clean_hosp =
+	hosp[19:509, hosp |> names() |> (\(x) !is.na(x))()] |>
 	(\(df) {
 		 df[, -c(1,2,12,14,16)] = lapply(df[, -c(1,2,12,14,16)], replace_star_integer)
 		 df[, c(12,14,16)] = lapply(df[, c(12,14,16)], replace_star_numeric)
 		 df[, "Code"] = lapply(df[, "Code"], replace_X_code)
 		 df
-#		 df |>
-#			 subset(`Finished consultant episodes` == "3") |>
-#			 print()
-		 # df[df$`Finished consultant episodes` %in% grep("3", df$`Finished consultant episodes`, value = TRUE), ]
 })() |>
 (\(df) {
 	 df[order(df$"Code"),]
 })() |>
+`[`(x = _, c("Code","Finished consultant episodes")) |> 
 print()
 
-clean_hosp |>
-	subset(Code %in% badcode) |>
-	`[`(x = _, c(1:3)) |> 
-	print()
+# ## Correspondance `Code` -- `Geographic.Local.Authority.Code` (Code NHS de l'établissement -- code NHS géographique) 
 
-clean_hosp[,"Code"] |>
-	print()
+# # Ajout manuel des hôpitaux "London Clinic" et "Harley Street On 15" qui manquent.
 
-# "Code" est le code NHS de l'établissement, et "Geographic.Local.Authority.Code" son code géographique NHS (pas ONS).
-nhs_geo_code = read.csv("ODS_2023-06-20T170741.csv") |>
-# ajout des codes pour "London Clinic" et "Harley Street On 15", qui n'ont pas de code dans le document original.
-# selon:
-# https://odsportal.digital.nhs.uk/Organisation/OrganisationDetails?organisationId=67432&showOpenChildredOnly=True
-# https://www.streetcheck.co.uk/postcode/w1g6bw
-# "8A718 London Clinic" a pour autorité administrative: Westminster (ONS: E09000033, NHS: 713)
-# selon:
-# https://odsportal.digital.nhs.uk/Organisation/OrganisationDetails?organisationId=75403&showOpenChildredOnly=True
-# https://www.streetcheck.co.uk/postcode/nw12bu
-# "8HE28 Harley Street On 15" a pour autorité administrative: Camden (ONS: E09000007, NHS: 702)
-	add_row(Code = "8A718",Geographic.Local.Authority.Code = "713",Geographic.Local.Authority.Name = "CITY OF WESTMINSTER") |>
-	add_row(Code = "8HE28",Geographic.Local.Authority.Code = "702",Geographic.Local.Authority.Name = "LONDON BOROUGH OF CAMDEN") |>
+nhs_geo_code =
+	read.csv("ODS_2023-06-20T170741.csv") |>
+	rbind(data.frame(
+			 Code = c("8A718","8HE28"),
+			 Geographic.Local.Authority.Code = c("713","702"),
+			 Geographic.Local.Authority.Name = c("CITY OF WESTMINSTER", "LONDON BOROUGH OF CAMDEN")
+			 )) |>
 (\(df) {
 	 df[order(df$"Code"),]
-})() |>
-	as_tibble() |>
-	print()
-
-# fichier de correspondance trouvé par hasard à : https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2020/09/LA-Type-B-February-2020-4W5PA.xls
-# d'autres fichiers de correspondance plus anciens existent
-nhs2ons_code = read_excel("LA-Type-B-February-2020-4W5PA.xls", sheet = "LA - by type of care") |>
-	print(n = 20)
-
-colnames(nhs2ons_code) = nhs2ons_code[12,] |>
-	gsub("Code","Geographic.Local.Authority.Code", x=_) |>
-	print()
-
-clean_nhs2ons_code = nhs2ons_code[15:164,c("ONS Geography", "Geographic.Local.Authority.Code")] |>
-(\(df) {
-	 df[order(df$"Geographic.Local.Authority.Code"),]
-})() |>
-	print()
-
-# fusion des fichiers nhs_geo_code et clean_hosp selon le code de l'établissement. permet d'associer le code NHS de l'établissement à son code géographique NHS.
-nhs_geo_code_clean_hosp = merge(nhs_geo_code, clean_hosp, by = "Code") |>
-	(\(df) {
-		 df[order(df$"Geographic.Local.Authority.Code"),]
-})() |>
-# fusion avec le fichier clean_nhs2ons_code. Permet d'associer le code géographique NHS au code géographique ONS.
-merge(clean_nhs2ons_code, by = "Geographic.Local.Authority.Code") |>
-`[`(x = _, c("ONS Geography", "Hospital.provider.name", "Admissions")) |> 
-# grouper par code géographique ONS et sommer les éléments de chaque groupe.
-(\(df) {
-	 df = aggregate(df$Admissions, by = df$`ONS Geography` |> list(), FUN = sum)
-	 names(df) = c("ONS.Geography", "Admission") 
-	 df
 })() |>
 as_tibble() |>
 print()
 
-help("assign")
+# ## Correspondance `Geographic.Local.Authority.Code` et `ONS Geography` (NHS geographic code -- ONS geographic code)
+
+nhs2ons_code =
+	read_excel("LA-Type-B-February-2020-4W5PA.xls", sheet = "LA - by type of care") |>
+	print(n = 20)
+
+colnames(nhs2ons_code) =
+	nhs2ons_code[12,] |>
+	gsub("Code","Geographic.Local.Authority.Code", x=_) |>
+	print()
+
+clean_nhs2ons_code =
+	nhs2ons_code[15:164,c("ONS Geography", "Geographic.Local.Authority.Code")] |>
+	(\(df) {
+		 df[order(df$"Geographic.Local.Authority.Code"),]
+})() |>
+print()
+
+# ## Fusion des fichiers clean_hosp et nhs_geo_code selon `Code` (le code de l'établissement)
+
+# Permet d'associer le code NHS de l'établissement à son code géographique NHS.
+
+nhs_geo_code_clean_hosp =
+	merge(nhs_geo_code, clean_hosp, by = "Code") |>
+	(\(df) {
+		 df[order(df$"Geographic.Local.Authority.Code"),]
+}
+	)() |>
+# fusion avec le fichier clean_nhs2ons_code. Permet d'associer le code géographique NHS au code géographique ONS.
+	merge(clean_nhs2ons_code, by = "Geographic.Local.Authority.Code") |>
+	`[`(x = _, c("ONS Geography", "Finished consultant episodes")) |> 
+# grouper par code géographique ONS et sommer les éléments de chaque groupe.
+	(\(df) {
+		 df = aggregate(df$`Finished consultant episodes`, by = df$`ONS Geography` |> list(), FUN = sum)
+		 names(df) = c("ONS.Geography", "Finished consultant episodes") 
+		 df
+	})() |>
+	as_tibble() |>
+	print()
+
+help("[.data.frame")
+
+help("data.frame")
+
+help("subset")
+
+data.frame(x = 1, y = 1:10) |>
+	as_tibble() |>
+	print()
+
+??parallel
+
+data.frame(col1 = c(1, 2, 3), col2 = c("a", "b", "c"), col3 = c(TRUE, FALSE, TRUE)) |>
+	subset(select = c(1,2)) |>
+	print()
+
+	`[`(x = _, 
+
+
+
+	subset(select = c(col1, col2))
